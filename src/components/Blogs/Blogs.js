a666e6ba5689cfa185e373b607cd5d37298235a4
AkashAbrrar <mabrrar9@gmail.com>
2022-04-04 21:54:50 +0600

    this commit for nab bar and links all the components

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>This page is for blogs and information</h2>
+        </div>
+    );
+};
+
+export default Blogs;
