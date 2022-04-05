1489ddede8ded896045eeea105bfd5cd9b76aed1
AkashAbrrar <mabrrar9@gmail.com>
2022-04-05 14:27:01 +0600

    all fancy charts are added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Chart from '../Chart/Chart';
+
+
+
 
 const Dashboard = () => {
+
     return (
         <div>
             <h2>This page is for charts </h2>
+            <Chart></Chart>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='container'>
             <h2>The page you looking for is not found</h2>
             <h1>404</h1>
+            <p>ooppsss!</p>
         </div>
     );
 };
